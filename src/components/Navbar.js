ce20c6dfcac06fa492bdb6438b727979c9a5548c
SREEHARI <sreehari2610@gmail.com>
2021-02-25 14:27:07 +0530

    Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+const Navbar = () => {
+    return (
+        <div className="Navbar">
+            <div className="Navbar-contents">
+                <div className="Navbar-circle"></div>
+                <div className="Navbar-text">
+              <div className="Navbar-Text-header"> Will & More</div>
+              <div className="Navbar-Text-body"> Peace of mind for you and your family </div>
+              </div>
+           </div>
+
+        </div>
+    )
+}
+
+export default Navbar
